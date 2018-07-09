90ee116221fa84e50d007de1c7ba3fa46f9661df
Greg <greg@Gregs-MacBook-Air.local>
2018-07-09 12:14:32 -0700

    setup start script and heroku port

---
+++ server.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + `\n`, (err) => {
+    if (err) {
+      console.log('Unable to append to server');
+    }
+  });
+  next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.render('main.hbs', {
+    name: 'Greg',
+    pageTitle: 'Main Page',
+    fortune: 'You are to be an excellent and well compensated software developer.',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear(),
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Custom error message',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+
+});
